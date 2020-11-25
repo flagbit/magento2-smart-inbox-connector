7993521c4b185f1977690f08b9f4c302467734d3
Michael Vogel <michael.vogel@flagbit.de>
2020-11-25 16:31:52 +0100

    [base] Override prepare of rows

--- Block/Adminhtml/Form/Field/OrderStatusMatrix.php
+++ Block/Adminhtml/Form/Field/OrderStatusMatrix.php
@@ -9,6 +9,9 @@ class OrderStatusMatrix extends AbstractFieldArray
     private const MAGE_STATUS_KEY = 'mage_status';
     private const SCHEMA_ORG_STATUS_KEY = 'schema_org_status';
 
+    /** @var SchemaOrgColumn $orderStatusRenderer */
+    private $orderStatusRenderer;
+
     /**
      * @inheritDoc
      */
@@ -30,5 +33,37 @@ class OrderStatusMatrix extends AbstractFieldArray
         );
         $this->_addAfter       = false;
         $this->_addButtonLabel = __('Not usable');
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected function _prepareArrayRow(DataObject $row)
+    {
+        $options     = [];
+        $orderStatus = $row->getData(self::SCHEMA_ORG_STATUS_KEY);
+        if ($orderStatus !== null) {
+            $options['option_' . $this->getSchemaOrgStatusRenderer()->calcOptionHash($orderStatus)] = 'selected="selected"';
+        }
+
+        $row->setData('option_extra_attrs', $options);
+    }
+
+    /**
+     * Get Schema orc status renderer
+     *
+     * @return SchemaOrgColumn
+     */
+    private function getSchemaOrgStatusRenderer()
+    {
+        if (!$this->orderStatusRenderer) {
+            $this->orderStatusRenderer = $this->getLayout()->createBlock(
+                SchemaOrgColumn::class,
+                '',
+                [ 'data' => [ 'is_render_to_js_template' => true ] ]
+            );
+        }
+
+        return $this->orderStatusRenderer;
     }
 }
