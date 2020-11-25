335012287482e29164aabcbed92a7e0c157f9cc4
Michael Vogel <michael.vogel@flagbit.de>
2020-11-25 16:15:04 +0100

    [base] Add columns to config matrix

--- Block/Adminhtml/Form/Field/OrderStatusMatrix.php
+++ Block/Adminhtml/Form/Field/OrderStatusMatrix.php
@@ -6,4 +6,26 @@ use Magento\Config\Block\System\Config\Form\Field\FieldArray\AbstractFieldArray;
 
 class OrderStatusMatrix extends AbstractFieldArray
 {
+    /**
+     * @inheritDoc
+     */
+    protected function _prepareToRender()
+    {
+        $this->addColumn(
+            'mage_status',
+            [
+                'label' => __('Magento Status'),
+                'class' => 'required-entry',
+            ]
+        );
+        $this->addColumn(
+            'schema_org_status',
+            [
+                'label'    => __('Schema.org Status'),
+                'renderer' => 'required-entry',
+            ]
+        );
+        $this->_addAfter       = false;
+        $this->_addButtonLabel = __('Not usable');
+    }
 }
