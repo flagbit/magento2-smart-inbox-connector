0e6a34924837e49af6408e021fb35b1470a3757c
Michael Vogel <michael.vogel@flagbit.de>
2020-11-25 16:11:01 +0100

    [base] Add OrderStatusMatrixConfig model

---
+++ Block/Adminhtml/Form/Field/OrderStatusMatrix.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Flagbit\TransactionMailExtender\Block\Adminhtml\Form\Field;
+
+use Magento\Config\Block\System\Config\Form\Field\FieldArray\AbstractFieldArray;
+
+class OrderStatusMatrix extends AbstractFieldArray
+{
+}
