eeacfcfe644a905922ccf3831429528de0a610a8
Michael Vogel <michael.vogel@flagbit.de>
2020-11-25 22:09:01 +0100

    [base] Add multiselectemail config model

---
+++ Block/Adminhtml/Form/Field/MultiSelectEmail.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Flagbit\TransactionMailExtender\Block\Adminhtml\Form\Field;
+
+use Magento\Email\Model\Template\Config;
+use Magento\Framework\Data\OptionSourceInterface;
+
+class MultiSelectEmail implements OptionSourceInterface
+{
+    /** @var Config $emailConfig */
+    private $emailConfig;
+
+    public function __construct(
+        Config $emailConfig
+    ) {
+        $this->emailConfig = $emailConfig;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function toOptionArray()
+    {
+        $result = [];
+        foreach ($this->getConfigTemplates() as $emailTemplate) {
+            $result[] = [
+                'value' => $emailTemplate['value'],
+                'label' => $emailTemplate['label'],
+            ];
+        }
+
+        return $result;
+    }
+
+    /**
+     * Get list of available email templates
+     *
+     * @return array[]
+     */
+    private function getConfigTemplates()
+    {
+        return $this->emailConfig->getAvailableTemplates();
+    }
+}
